ab3c96d2e2d10f2a68fe69ca5f4c27308fb07f10
RubenTG11 <60044303+RubenTG11@users.noreply.github.com>
2024-04-16 16:09:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -26,17 +26,17 @@ async function updateTable() {
     var group = parent.children[2].textContent.toLowerCase();
 
     if (title.includes(search.toLowerCase()) || group == search.toLowerCase() || (isInt(search) && parseInt(search) == parseInt(year.substring(0, search.length)))) {
-      if(element.classList.contains("card-hide")){
+      if (element.classList.contains("card-hide")) {
         element.classList.remove("card-hide");
-        
+
       }
     } else {
-      if(!element.classList.contains("card-hide")) {
+      if (!element.classList.contains("card-hide")) {
         element.classList.toggle("card-hide");
         console.log(title);
       }
     }
-    
+
 
   }
 
@@ -46,9 +46,9 @@ async function updateTable() {
 
 
 
- async function fillTable() {
-  
- 
+async function fillTable() {
+
+
   fetch("./data.json")
     .then(function (response) {
       return response.json();
@@ -91,106 +91,70 @@ async function updateTable() {
 
           if (titel.includes(search.toLowerCase()) || data[i].Trommlergruppe.toLowerCase() == search.toLowerCase() || (isInt(search) && parseInt(search) == parseInt(data[i].Jahrgang.toString().substring(0, search.length)))) {
 
-        
+
             var finalTitel = titelTable;
             var year = data[i].Jahrgang;
             var group = data[i].Trommlergruppe;
             var type = (data[i].Typ == "Locks" ? "Lock" : data[i].Typ);
             var logoLink = "";
-            var piper = (data[i].Pferferstimme == null || data[i].Pferferstimme == "")  ? null : data[i].Pferferstimme;
+            var piper = (data[i].Pferferstimme == null || data[i].Pferferstimme == "") ? null : data[i].Pferferstimme;
             var drums = (data[i].Trommlerstimme == null || data[i].Trommlerstimme == "") ? null : data[i].Trommlerstimme;
             var audioLink = (data[i].audioLink == null || data[i].audioLink == "") ? null : data[i].audioLink
             var link = (data[i].Link == null || data[i].Link == "") ? "https://rubentg11.github.io/LockSuche" : data[i].Link;
 
-            if(data[i].imgLink == ""){
+            if (data[i].imgLink == "") {
               logoLink = "./img/Logo_dummy.jpg";
-            }else if(group == "FSG"){
+            } else if (group == "FSG") {
               logoLink = "./img/Logo_FSG.jpg";
-            }else if(group == "Turmfalken"){
+            } else if (group == "Turmfalken") {
               logoLink = "./img/Logo_Tufas.jpg";
-            }else{
-              logoLink = "./img/Logo_"+year+".jpg";
+            } else {
+              logoLink = "./img/Logo_" + year + ".jpg";
             }
-           
+
             toInsert2 += lockCard(finalTitel, year, group, type, logoLink, typeColor, groupColor, "rgb(122, 121, 121, 0.5)", piper, drums, audioLink, link);
           }
 
 
-          
+
         }
 
-        
-  
-        
+
+
+
       }
 
       document.getElementById('newArea').innerHTML = toInsert2;
 
-      
 
-var coll = document.getElementsByClassName("drumsbutton");
+      document.querySelector(".newArea").addEventListener("click", (e) => {
 
-for (let i = 0; i < coll.length; i++) {
-    const element = coll[i];
+        var element = e.target;
 
-    element.addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = document.getElementById(element.getAttribute('to-open'));
-        if (content.style.display === "block") {
-          content.style.display = "none";
-          document.getElementById("plus-"+element.getAttribute('to-open')).innerHTML = "&#10133";
-        } else {
-          content.style.display = "block";
-          document.getElementById("plus-"+element.getAttribute('to-open')).innerHTML = "&#10134";
-        } 
-      });
-    
-}
-  
+        if (element.nodeName == "BUTTON" || element.id.includes("plus")) {
 
-  var coll2 = document.getElementsByClassName("piperbutton");
 
-  for (let i = 0; i < coll2.length; i++) {
-    const element = coll2[i];
+          var content = document.getElementById(element.getAttribute('to-open'));
+          var button = content == null ? element : element.firstChild;
 
-    element.addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = document.getElementById(element.getAttribute('to-open'));
-        if (content.style.display === "block") {
-          content.style.display = "none";
-          document.getElementById("plus2-"+element.getAttribute('to-open')).innerHTML = "&#10133";
-        } else {
-          content.style.display = "block";
-          document.getElementById("plus2-"+element.getAttribute('to-open')).innerHTML = "&#10134";
+          content = content == null ? document.getElementById(element.parentElement.getAttribute('to-open')) : content;
+
+          if (content.style.display === "block") {
+            content.style.display = "none";
+            button.innerHTML = "&#10133";
+          } else {
+            content.style.display = "block";
+            button.innerHTML = "&#10134";
+          }
         }
       });
-    
-  }
 
 
 
-  var coll3 = document.getElementsByClassName("audiobutton");
 
-  for (let i = 0; i < coll3.length; i++) {
-    const element = coll3[i];
-    
-    element.addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = document.getElementById(element.getAttribute('to-open'));
-        if (content.style.display === "block") {
-          
-          content.style.display = "none";
-          document.getElementById("plus3-"+element.getAttribute('to-open')).innerHTML = "&#10133";
-        } else {
-          content.style.display = "block";
-          document.getElementById("plus3-"+element.getAttribute('to-open')).innerHTML = "&#10134";
-        }
-      });
-  }
 
-    
 
-      
+
 
 
 
@@ -199,11 +163,11 @@ for (let i = 0; i < coll.length; i++) {
       console.error(error);
     });
 
-    await delay(2000);
-    document.querySelector('.loadingImg').classList.toggle("card-hide");
-    document.querySelector('.wrapper').classList.remove("card-hide");
-    
-  
+  await delay(2000);
+  document.querySelector('.loadingImg').classList.toggle("card-hide");
+  document.querySelector('.wrapper').classList.remove("card-hide");
+
+
 
 }
 
@@ -211,58 +175,60 @@ function isInt(n) {
   return /^[+-]?\d+$/.test(n);
 }
 
-function lockCard(title, year, group, type, logoLink, typecolor, groupcolor, yearcolor, piper, drums, audioLink, link){
+function lockCard(title, year, group, type, logoLink, typecolor, groupcolor, yearcolor, piper, drums, audioLink, link) {
   var lockCard = "";
+
+  link = (link == null || link == "") ? "rubentg11.github.io/LockSuche" : link;
 
 
   lockCard += `<div class="card">
         <div class="avatar">
-        <img class="avatarimg" src="`+logoLink+`" loading="lazy" alt="oh no">
+        <img class="avatarimg" src="`+ logoLink + `" loading="lazy" alt="oh no">
         </div>
         <div class="cardcontent" id="cardcontent">
             <div class="title">
                 <p>
-                        <a class="titletext" href="`+link+`">`+title+`</a> 
-                        <a class="jahrgang" style="background-color: ` + yearcolor + `;">`+year+`</a>
-                        <a class="group" style="background-color: ` + groupcolor + `;">`+group+`</a>
-                        <a class="type" style="background-color: ` + typecolor + `;">`+type+`</a>
+                        <a class="titletext" href="`+ link + `">` + title + `</a> 
+                        <a class="jahrgang" style="background-color: ` + yearcolor + `;">` + year + `</a>
+                        <a class="group" style="background-color: ` + groupcolor + `;">` + group + `</a>
+                        <a class="type" style="background-color: ` + typecolor + `;">` + type + `</a>
                 </p>
             </div>`
 
-            var openclass = title.replaceAll(" ", "").replaceAll(".", "").replaceAll(",", "").replaceAll("-", "").replaceAll("–","");
+  var openclass = title.replaceAll(" ", "").replaceAll(".", "").replaceAll(",", "").replaceAll("-", "").replaceAll("–", "");
 
-            if(drums != null) {
-              
-              lockCard += `<div class="drums">
+  if (drums != null) {
+
+    lockCard += `<div class="drums">
 
                   <p class="drumstitle">
-                      <button type="button" class="collapsible drumsbutton" to-open="drum-`+openclass+`" id="drumsbutton"><a id="plus-drum-`+openclass+`">&#10133</a> Trommeln</button>
+                      <button type="button" class="collapsible drumsbutton" to-open="drum-`+ openclass + `" id="drumsbutton"><a id="plus-drum-` + openclass + `">&#10133</a> Trommeln</button>
                   </p>
 
-                  <p class="drumstext" id="drum-`+openclass+`">
-                  `+drums+`
-                  </p>
-              </div>`
-            }
-
-            if(piper != null) {
-              lockCard += `<div class="piper">
-                  <p class="pipertitle" id="pipertitle"><button type="button" class="collapsible2 piperbutton" id="piperbutton" to-open="piper-`+openclass+`"><a id="plus2-piper-`+openclass+`">&#10133</a> Pfeifen</button></p>
-                  <p id="piper-`+openclass+`" class="pipertext">
-                  `+piper+`
+                  <p class="drumstext" id="drum-`+ openclass + `">
+                  `+ drums + `
                   </p>
               </div>`
-            }
+  }
 
-            if(audioLink != null) {
-            lockCard += `<div class="audio">
+  if (piper != null) {
+    lockCard += `<div class="piper">
+                  <p class="pipertitle" id="pipertitle"><button type="button" class="collapsible2 piperbutton" id="piperbutton" to-open="piper-`+ openclass + `"><a id="plus2-piper-` + openclass + `">&#10133</a> Pfeifen</button></p>
+                  <p id="piper-`+ openclass + `" class="pipertext">
+                  `+ piper + `
+                  </p>
+              </div>`
+  }
+
+  if (audioLink != null) {
+    lockCard += `<div class="audio">
                 <p class="audioTitle">
-                    <button type="button" class="collapsible3 audiobutton" id="audiobutton" to-open="audio-`+openclass+`"><a id="plus3-audio-`+openclass+`">&#10133</a> Audio</button>
+                    <button type="button" class="collapsible3 audiobutton" id="audiobutton" to-open="audio-`+ openclass + `"><a id="plus3-audio-` + openclass + `">&#10133</a> Audio</button>
                 </p>
-                <audio id="audio-`+openclass+`" class="audioelement" controls="controls" preload="none" data-preload="metadata"><source src="`+audioLink+`" type="audio/mpeg"><span style="color: red;">Dein Browser unterstützt das Audio-Element leider nicht.</span></audio>
+                <audio id="audio-`+ openclass + `" class="audioelement" controls="controls" preload="none" data-preload="metadata"><source src="` + audioLink + `" type="audio/mpeg"><span style="color: red;">Dein Browser unterstützt das Audio-Element leider nicht.</span></audio>
             </div>`
-            }
-        lockCard += `</div> </div>`
+  }
+  lockCard += `</div> </div>`
 
   return lockCard;
 } 
